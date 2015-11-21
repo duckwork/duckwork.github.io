50610bfdc2cb921768cfc4d1fe873098491a8f03
Case Duckworth <cased123@gmail.com>
2015-11-20 19:24:02 -0600

    Add publishing script

---
+++ publi.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Small script to publish a new post.
+
+force="${1:-n}"
+
+if ! make -q; then
+  make
+elif [[ "$force" == "-f" ]]; then
+  make again
+fi
+
+newest_post="$(ls -1 --sort=time p/*.txt | head -n1)" # TODO: something better
+newest_title="$(head -n1 "${newest_post}" | tr -d '%')"
+
+git commit -ame "Publish ${newest_title}"
+git push
