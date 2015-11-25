ec2a7b478765e34746a294c0614c50c5667ab304
Case Duckworth <cased123@gmail.com>
2015-11-24 21:59:30 -0600

    Add more options to publi.sh

--- publi.sh
+++ publi.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 # Small script to publish a new post.
 
-arg="${1:-n}"
-
-# TODO: add switches to enable different commit message; no-web option
+force=false; promptMsg=false; editMsg=false; offline=false; dryrun=false;
+while (( "$#" > 0 )); do
+  case "$1" in
+    -f ) force=true     ;;
+    -m ) promptMsg=true ;;
+    -M ) editMsg=true   ;;
+    -n ) offline=true   ;;
+  esac
+  shift;
+done
 
 if ! make -q; then
   make
-elif [[ "$arg" == "-f" ]]; then
+elif $force; then
   make again
 fi
 
@@ -15,4 +22,14 @@ newest_post="$(ls -1 --sort=time p/*.txt | head -n1)" # TODO: something better
 newest_title="$(head -n1 "${newest_post}" | tr -d '%')"
 
 git add .
-git commit -m "Publish:${newest_title}" && git push
+if ! $promptMsg; then
+  git commit -m "Publish:${newest_title}"
+elif ! $editMsg; then
+  echo -n "Commit message: "; read msg
+  git commit -m "$msg"
+else
+  git commit
+fi
+if ! $offline && (( $? == 0 )); then
+  git push
+fi
