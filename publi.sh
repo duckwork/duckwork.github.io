5d7a1287a5c933bfd22adb3c55bf33b568042727
Case Duckworth <cased123@gmail.com>
2015-11-24 22:01:15 -0600

    Publish: A failure blog

--- publi.sh
+++ publi.sh
@@ -13,14 +13,18 @@ while (( "$#" > 0 )); do
 done
 
 if ! make -q; then
+  echo "Making..."
   make
 elif $force; then
+  echo "Making again..."
   make again
 fi
 
+echo "Collating..."
 newest_post="$(ls -1 --sort=time p/*.txt | head -n1)" # TODO: something better
 newest_title="$(head -n1 "${newest_post}" | tr -d '%')"
 
+echo "Committing..."
 git add .
 if ! $promptMsg; then
   git commit -m "Publish:${newest_title}"
